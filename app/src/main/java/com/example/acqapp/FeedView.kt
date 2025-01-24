3f35153acbab5edad6bc6758fa336097ffcaf1fd
Муса Зарифянов <zarifanovmusa@gmail.com>
2025-01-24 16:52:25 +0300

    Feed + TabBar

--- app/src/main/java/com/example/acqapp/FeedView.kt
+++ app/src/main/java/com/example/acqapp/FeedView.kt
@@ -1,2 +1,175 @@
 package com.example.acqapp
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.acqapp.ui.theme.AcqAppTheme
+
+@Composable
+fun FeedView(
+    modifier: Modifier = Modifier,
+    name: String,
+    age: Int,
+    userStatus: String
+) {
+    Box(
+        modifier = modifier
+            .background(colorResource(R.color.main_color))
+    ) {
+        Column(
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(5.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 10.dp, end = 10.dp)
+            ) {
+                Text(
+                    text = name,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = colorResource(R.color.text_feed_color)
+                )
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                Text(
+                    text = "$age лет",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = colorResource(R.color.text_feed_color)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Text(
+                text = userStatus,
+                fontSize = 17.sp,
+                fontWeight = FontWeight.Normal,
+                color = colorResource(R.color.text_feed_color),
+                modifier = Modifier.padding(start = 5.dp, end = 5.dp),
+                maxLines = Int.MAX_VALUE,
+                overflow = TextOverflow.Visible
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun FeedViewPreview() {
+    AcqAppTheme() {
+        FeedView(Modifier.width(305.dp).height(560.dp), "Musa", 18, "Львица в самом соку")
+    }
+}
+
+@Composable
+fun TabBar(modifier: Modifier = Modifier) {
+    NavigationBar(
+        modifier = modifier,
+        containerColor = colorResource(id = R.color.main_color)
+    ) {
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = "Home"
+                )
+            },
+            selected = true,
+            onClick = {}
+        )
+
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = "Profile"
+                )
+            },
+            selected = false,
+            onClick = {}
+        )
+
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Email,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = "Messages"
+                )
+            },
+            selected = false,
+            onClick = {}
+        )
+
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Settings,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = "Settings"
+                )
+            },
+            selected = false,
+            onClick = {}
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun TabBarPreview() {
+    AcqAppTheme {
+        TabBar()
+    }
+}
